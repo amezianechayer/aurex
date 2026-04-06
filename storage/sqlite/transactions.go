2fff8473cc4369c509b759c4f9256f861d100325
amezianechayer <amezianechayer9@gmail.com>
2026-04-06 10:42:07 -0400

    storage: fix invalid ref field handling with non-NULL entries

--- storage/sqlite/transactions.go
+++ storage/sqlite/transactions.go
@@ -70,20 +70,29 @@ func (s *SQLiteStore) FindTransactions(q query.Query) (query.Cursor, error) {
 		var txid int64
 		var ts string
 		var thash string
-		var tref string
+
+		var ref interface{}
 
 		posting := core.Posting{}
 
-		rows.Scan(
+		err := rows.Scan(
 			&txid,
 			&ts,
 			&thash,
-			&tref,
+			&ref,
 			&posting.Source,
 			&posting.Destination,
 			&posting.Amount,
 			&posting.Asset,
 		)
+		if err != nil {
+			return c, err
+		}
+
+		var refStr string
+		if ref != nil {
+			refStr = ref.(string)
+		}
 
 		if _, ok := transactions[txid]; !ok {
 			transactions[txid] = core.Transaction{
@@ -91,7 +100,7 @@ func (s *SQLiteStore) FindTransactions(q query.Query) (query.Cursor, error) {
 				Postings:  []core.Posting{},
 				Timestamp: ts,
 				Hash:      thash,
-				Reference: tref,
+				Reference: refStr,
 				Metadata:  core.Metadata{},
 			}
 		}
