885e3937be4a549ee9ef4c1456cdf9660434874c
amezianechayer <amezianechayer9@gmail.com>
2026-04-08 15:55:37 -0400

    feat: implement new API structure with actions controllers  and middleware for ledger management

---
+++ api/actions/script_controller.go
@@ -0,0 +1,46 @@
+package actions
+
+import (
+	"strings"
+
+	"github.com/amezianechayer/corren/core"
+	"github.com/amezianechayer/corren/ledger"
+	"github.com/gin-gonic/gin"
+)
+
+// ScriptController -
+type ScriptController struct {
+	BaseController
+}
+
+// NewScriptController -
+func NewScriptController() *ScriptController {
+	return &ScriptController{}
+}
+
+// CreateScriptController -
+func CreateScriptController() *ScriptController {
+	return NewScriptController()
+}
+
+// PostScript -
+func (ctl *ScriptController) PostScript(c *gin.Context) {
+	l, _ := c.Get("ledger")
+
+	var script core.Script
+	c.ShouldBind(&script)
+
+	err := l.(*ledger.Ledger).Execute(script)
+
+	res := gin.H{
+		"ok": err == nil,
+	}
+
+	if err != nil {
+		err_str := err.Error()
+		err_str = strings.ReplaceAll(err_str, "\n", "\r\n")
+		res["err"] = err_str
+	}
+
+	c.JSON(200, res)
+}
---
+++ api/router.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"github.com/amezianechayer/corren/api/actions"
+	"github.com/amezianechayer/corren/api/middlewares"
+	"github.com/amezianechayer/corren/ledger"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+// Router -
+func Router(cc cors.Config, resolver *ledger.Resolver) *gin.Engine {
+	router := gin.Default()
+
+	// Default Middlewares
+	router.Use(
+		cors.New(cc),
+		gin.Recovery(),
+		middlewares.AuthMiddleware(router),
+	)
+
+	// API Controllers
+	baseController := actions.CreateBaseController()
+	ledgerController := actions.CreateLedgerController()
+	transactionController := actions.CreateTransactionController()
+	accountController := actions.CreateAccountController()
+	scriptController := actions.CreateScriptController()
+	// API Routes
+	router.GET("/_infos", baseController.GetInfos)
+
+	ledgerGroup := router.Group("/:ledger", middlewares.LedgerMiddleware(resolver))
+	{
+		ledgerGroup.GET("/stats", ledgerController.GetStats)
+
+		transactionGroup := ledgerGroup.Group("/transactions")
+		{
+			transactionGroup.GET("/", transactionController.GetTransactions)
+			transactionGroup.POST("/", transactionController.PostTransaction)
+			transactionGroup.POST("/:transactionId/revert", transactionController.RevertTransaction)
+			transactionGroup.GET("/:transactionId/metadata", transactionController.GetTransactionMetadata)
+		}
+
+		accountGroup := ledgerGroup.Group("/accounts")
+		{
+			accountGroup.GET("/", accountController.GetAccounts)
+			accountGroup.GET("/:accountId", accountController.GetAddress)
+			accountGroup.GET("/:accountId/metadata", accountController.GetAccountMetadata)
+		}
+
+		scriptGroup := ledgerGroup.Group("/script")
+		{
+			scriptGroup.POST("/", scriptController.PostScript)
+		}
+	}
+
+	return router
+}
