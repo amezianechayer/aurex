aecdab96881e641f42f5c72fdcdcccbf0b9ad5d7
amezianechayer <amezianechayer9@gmail.com>
2026-04-13 09:14:27 -0400

    Add test actions

---
+++ api/actions/ledger_controller_test.go
@@ -0,0 +1,16 @@
+package actions
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewLedgerController(t *testing.T) {
+	if reflect.TypeOf(NewLedgerController()) != reflect.TypeOf(&LedgerController{}) {
+		t.Errorf(
+			"LedgerController return type is %s, should be %s",
+			reflect.TypeOf(NewLedgerController()),
+			reflect.TypeOf(&LedgerController{}),
+		)
+	}
+}
---
+++ api/actions/script_controller_test.go
@@ -0,0 +1,16 @@
+package actions
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewScriptController(t *testing.T) {
+	if reflect.TypeOf(NewScriptController()) != reflect.TypeOf(&ScriptController{}) {
+		t.Errorf(
+			"ScriptController return type is %s, should be %s",
+			reflect.TypeOf(NewScriptController()),
+			reflect.TypeOf(&ScriptController{}),
+		)
+	}
+}
